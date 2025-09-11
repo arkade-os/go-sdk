12d3445850c852ef6730ea04251d25feca5b8ff8
altafan <18440657+altafan@users.noreply.github.com>
2025-09-11 14:51:22 +0200

    Clean

--- client/rest/client.go
+++ client/rest/client.go
@@ -130,7 +130,7 @@ func (a *restClient) GetInfo(
 		UtxoMaxAmount:           int64(utxoMaxAmount),
 		VtxoMinAmount:           int64(vtxoMinAmount),
 		VtxoMaxAmount:           int64(vtxoMaxAmount),
-		CheckpointTapscript:     resp.Payload.CheckpointTapscript,
+		// CheckpointTapscript:     resp.Payload.CheckpointTapscript,
 	}, nil
 }
 
