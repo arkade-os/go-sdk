044ce979620b3c8e7708b4ce7eefafc57e42cc92
Bob Smith <5396652+bitcoin-coder-bob@users.noreply.github.com>
2025-12-24 12:41:10 -0500

    temporary logging

--- client/rest/client.go
+++ client/rest/client.go
@@ -349,6 +349,7 @@ func (c *restClient) GetEventStream(
 					Signature:  e.GetSignature(),
 				}
 			case !ark_service.IsNil(event.GetStreamStarted()):
+				fmt.Printf("--- case rest client StreamStartedEvent hit\n")
 				e := event.GetStreamStarted()
 				batchEvent = client.StreamStartedEvent{
 					Id: e.GetId(),
