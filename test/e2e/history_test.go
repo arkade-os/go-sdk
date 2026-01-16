2ca520507d2a886050cbe7fa8b24f372260c23bb
altafan <18440657+altafan@users.noreply.github.com>
2026-01-16 13:24:52 +0100

    Fix

--- test/e2e/history_test.go
+++ test/e2e/history_test.go
@@ -198,7 +198,7 @@ func TestTransactionHistory(t *testing.T) {
 	require.Empty(t, collabExitTx.ArkTxid)
 
 	// Give time to update also the other records
-	time.Sleep(time.Second)
+	time.Sleep(2 * time.Second)
 
 	history, err = alice.GetTransactionHistory(ctx)
 	require.NoError(t, err)
